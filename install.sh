797601d09273b97233f47aa3ed25109de863ee53
samwang0723 <sam.wang.0723@gmail.com>
2023-11-08 11:37:08 +0800

    feat: install opencv

---
+++ install.sh
@@ -0,0 +1,3 @@
+!/bin/bash
+
+apk --no-cache add bash build-base ca-certificates clang-dev clang cmake coreutils curl freetype-dev ffmpeg-dev ffmpeg-libs gcc g++ git gettext lcms2-dev libavc1394-dev libc-dev libffi-dev libjpeg-turbo-dev libpng-dev libressl-dev libtbb@edgetest libtbb-dev@edgetest libwebp-dev linux-headers make musl openblas@edgecomm openblas-dev@edgecomm openjpeg-dev openssl python3 python3-dev tiff-dev unzip zlib-dev
